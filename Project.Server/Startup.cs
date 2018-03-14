bfc6c5d942198d8837e7e94112b496a8cd8e75ce
Sravy <svelaga0412@gmail.com>
2018-03-14 10:43:10 -0700

    replace startup.cs signalr code

--- Project.Server/Startup.cs
+++ Project.Server/Startup.cs
@@ -12,18 +12,7 @@ namespace Project.Server
     {
         public void Configuration(IAppBuilder app)
         {
-            app.Map("/signalr", map =>
-            {
-
-                map.UseCors(CorsOptions.AllowAll);
-
-                var hubConfiguration = new HubConfiguration
-                {
-
-                };
-
-                map.RunSignalR(hubConfiguration);
-            });
+            app.MapSignalR();
         }
     }
 }
